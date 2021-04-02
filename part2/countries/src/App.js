d0037c6b1e1d0ed217265509cb7017fc0a2fd179
rdfiii <foulkesr@gmail.com>
2021-04-02 10:08:34 -0400

    CountryOfOne component displays detailed data for single item search

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -22,45 +22,53 @@ const App = () => {
   }
 
   const handleUpdatingCountries = () => {
-    const filteredNames = [];
+    const filteredCountries = [];
     
-      countries.map( country => country.name)
-      .map( function(name, i) { 
-        if(name.toLowerCase().includes(searchTerm.toLowerCase())){
-          return filteredNames.push(name)
-        } })
-        
-        return filteredNames.map(name => {
-          console.log(name)
-          return <li key={name}>{name}</li>
-          
+      countries.map( country => {
+        if(country.name.toLowerCase().includes(searchTerm.toLowerCase())){
+          return filteredCountries.push(country)
+        }
+      })
+      
+      if (filteredCountries.length === countries.length) {
+        return <h2>Search countries</h2>
+      } else if (filteredCountries.length > 10) {
+        return <p>Too many matches, specify another filter</p>
+      } else if (filteredCountries.length === 1) {
+        console.log(filteredCountries[0])
+        return <CountryOfOne 
+                  name={filteredCountries[0].name} 
+                  capital={filteredCountries[0].capital} 
+                  population={filteredCountries[0].population}
+                  languages={filteredCountries[0].languages.map(l => <li key={l.name}>{l.name}</li> )}
+                  flag={filteredCountries[0].flag}
+                />
+      } else {
+        return filteredCountries.map( country => {
+          return <li key={country.name}>{country.name}</li>
         })
-        
-  }
-
-  // const handleUpdatingCountries = () => {
-  //     console.log(selectedCountries);
-  //     countries.map( country => country.name)
-  //     .map( function(name, i) { 
-  //       if(name.includes(searchTerm)){
-  //         setSelectedCountries(selectedCountries + name)
-  //       } })
-  //       console.log(selectedCountries);
-        
-  // }
-
-  
+      }
+    }
+ 
 
   return (
     <div>
       <SearchForm handleFormChange={handleFormChange} />
-      <ul>
       <CountryDisplay names={handleUpdatingCountries()}/>
-      </ul>
-      
     </div>
   );
 }
+
+const CountryOfOne = ({ name, capital, population, languages, flag }) => (
+  <div>
+    <h2>{name}</h2>
+    <p>Capital: {capital}</p>
+    <p>Population: {population}</p>
+    <h3>Languages</h3>
+    <ul>{languages}</ul>
+    <img src={flag} />
+  </div>
+)
 
 const SearchForm = ({ handleFormChange }) => (
   <div>
@@ -75,9 +83,7 @@ const SearchForm = ({ handleFormChange }) => (
 )
 
 const CountryDisplay = (props) => (
-  <div>
-    <p>{props.names}</p>
-  </div>
+  props.names
 )
 
 
